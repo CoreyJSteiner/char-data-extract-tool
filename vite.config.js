0947fafdcea82ddce69ed38ac668b30a29799e2b
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-06-08 15:44:39 -0400

    updates configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/5e-2024-pdf-extract/',
   optimizeDeps: {
     include: ['pdfjs-dist']
+  },
+  build: {
+    assetsInlineLimit: 0
   }
 })
